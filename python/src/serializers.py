5822f6041722d4de8016c088e258b0994755c0ea
Brendan Berg <brendan@berg.industries>
2022-09-12 00:07:44 -0400

    add html serializer

--- python/src/serializers.py
+++ python/src/serializers.py
@@ -27,6 +27,7 @@ class Serializer(object):
     def serialize(cls, table):
         raise NotImplementedError()
 
+
 class TabloSerializer(Serializer):
     @classmethod
     def serialize(cls, table):
@@ -59,3 +60,58 @@ class TabloSerializer(Serializer):
             return 'true' if item else 'false'
         elif item is None:
             return '-'
+
+
+class HtmlSerializer(Serializer):
+    @classmethod
+    def serialize(cls, table: Table):
+        header = cls.serialize_header(table)
+        data = cls.serialize_data(table)
+
+        return f'<table>{header}{data}\n</table>'
+
+    @classmethod
+    def serialize_header(cls, table: Table):
+        if not table.header:
+            return ''
+        
+        def serialize_item(index: int, item):
+            col = int_to_alpha(index)
+            value = cls.serialize_item(item)
+            return f'<td data-col-index="{col}">{value}</td>'
+
+        items = (serialize_item(index, item) for index, item in enumerate(table.header))
+
+        return f'\n  <thead><tr>\n    {"\n    ".join(items)}\n  </tr></thead>'
+
+    @classmethod
+    def serialize_data(cls, table: Table):
+        def serialize_item(col_idx, row_idx, item):
+            col_str = int_to_alpha(col_idx)
+            value = cls.serialize_item(item)
+
+            if props := table.format and table.format.get_props(col_str, row_idx):
+                class_attr = f' class="{" ".join(props)}"'
+            else:
+                class_attr = ''
+
+            return f'<td data-col-index="{col_str}"{class_attr}>{value}</td>'
+
+        def serialize_row(row_idx, row):
+            items = (serialize_item(col_idx, row_idx, item) for col_idx, item in enumerate(row))
+            return f'<tr data-row-index="{row_idx}">\n      {"\n      ".join(items)}\n    </tr>'
+        
+        rows = (serialize_row(index, row) for index, row in enumerate(table.data))
+
+        return f'\n  <tbody>\n    {"\n    ".join(rows)}\n  </tbody>'
+
+    @classmethod
+    def serialize_item(cls, item):
+        if isinstance(item, str):
+            return item
+        elif isinstance(item, (int, float)):
+            return str(item)
+        elif isinstance(item, bool):
+            return 'True' if item else 'False'
+        elif item is None:
+            return ''
